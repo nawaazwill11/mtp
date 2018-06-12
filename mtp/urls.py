441777fec2fd6a1729b53bbf604827d9ed8163ad
nawaazwill11 <mastermindjim@gmail.com>
2018-06-12 10:50:07 +0530

    Iniital commit

---
+++ mtp/urls.py
@@ -0,0 +1,8 @@
+from django.conf.urls import re_path
+from mtp import views
+from django.views.generic import TemplateView
+
+indexpatterns = [
+	re_path(r'^$', views.IndexPageView.as_view(), name='index'),
+	re_path(r'test/', TemplateView.as_view(template_name='test.html'))
+]
