ddd665794b7d4a46a4855f3d2d233890446efdac
nawaazwill11 <mastermindjim@gmail.com>
2018-06-12 13:19:05 +0530

    index made

--- mtp/urls.py
+++ mtp/urls.py
@@ -4,5 +4,6 @@ from django.views.generic import TemplateView
 
 indexpatterns = [
 	re_path(r'^$', views.IndexPageView.as_view(), name='index'),
-	re_path(r'test/', TemplateView.as_view(template_name='test.html'))
+	re_path(r'test/', TemplateView.as_view(template_name='test.html'), name='test'),
+	re_path(r'about/',TemplateView.as_view(template_name='about.html'), name='about')
 ]
--- mtpApp/urls.py
+++ mtpApp/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import re_path, include
 from mtp import urls
@@ -20,3 +22,6 @@ from mtp import urls
 urlpatterns = [
     re_path(r'^', include(urls.indexpatterns))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
